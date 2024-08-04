fa82414f0b29741848c1f03a702b8eab34d044c7
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-04 21:05:39 +0100

    worked on register user endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,21 @@
+const User = require("../model/User");
 const register = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    return res.status(400).json({ msg: "User already exists" });
+  }
+
+  const newUser = await User.create(req.body);
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      email: newUser.email,
+    });
+  } else {
+    res.status(400).json({ msg: "Invalid user data" });
+  }
+
   res.status(200).json({ msg: "Register user" });
 };
 const login = async (req, res) => {
---
+++ model/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const UserSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: [true, "Provide user full name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Provide user email"],
+    unique: [true, "email already exists"],
+  },
+  password: {
+    type: String,
+    required: [true, "Provide user password"],
+  },
+});
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
