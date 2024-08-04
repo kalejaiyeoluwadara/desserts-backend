dc4ec7b37a91b6daab6687aef952c17ecf9e455b
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-08-04 22:32:11 +0100

    fixed getSingleOrder bug

--- controllers/Order.js
+++ controllers/Order.js
@@ -11,6 +11,33 @@ const createOrder = async (req, res) => {
       .json({ message: "Failed to create order", error: error.message });
   }
 };
+const getAllOrders = async (req, res) => {
+  const orders = await Order.find({});
+  if (orders) {
+    res.status(200).json({ msg: orders });
+  } else {
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+};
+const getSingleOrder = async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    return res
+      .status(400)
+      .json({ message: "Bad request: Order ID is required" });
+  }
+  try {
+    const order = await Order.findById(id);
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    res.status(200).json({ order });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
 
 // Update an existing order
 const updateOrder = async (req, res) => {
@@ -51,4 +78,6 @@ module.exports = {
   createOrder,
   updateOrder,
   deleteOrder,
+  getSingleOrder,
+  getAllOrders,
 };
--- routers/order.js
+++ routers/order.js
@@ -3,9 +3,12 @@ const {
   createOrder,
   updateOrder,
   deleteOrder,
+  getSingleOrder,
+  getAllOrders,
 } = require("../controllers/Order");
 const router = express.Router();
-router.post("/", createOrder);
-router.route("/:orderId").put(updateOrder).delete(deleteOrder);
+router.route("/").get(getAllOrders).post(createOrder);
+router.get("/:orderId", getSingleOrder);
+router.route("/:orderId").patch(updateOrder).delete(deleteOrder);
 
 module.exports = router;
